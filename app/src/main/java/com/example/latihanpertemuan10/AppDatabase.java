51a30f76664594908b55628712d60ade0e98f47d
pukis <fzainulfikri@email.unikom.ac.id>
2020-05-01 20:27:26 +0700

    jadi

---
+++ app/src/main/java/com/example/latihanpertemuan10/AppDatabase.java
@@ -0,0 +1,9 @@
+package com.example.latihanpertemuan10;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {AktivisEntity.class},version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract AktivisDao aktivisDao();
+}
